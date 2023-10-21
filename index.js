0c8a3e61574bba5e921beaf48ee275383266c0d6
Jorge Maestre <maestrejorgeluis@hotmail.com>
2023-10-21 01:03:38 -0500

    first commit1

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 
   const server = require("./src/app.js")
-  const connectToDatabase = require("./src/db.js")
   const PORT = process.env.PORT || 3001;
   server.listen(PORT, async () => {
-     await connectToDatabase()
     console.log(`Servidor escuchando en el puerto ${PORT}`);
     });
